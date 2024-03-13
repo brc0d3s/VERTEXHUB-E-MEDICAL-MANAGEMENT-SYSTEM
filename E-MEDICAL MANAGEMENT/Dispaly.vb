630c6338483d08790cee158773665a2349989ddf
brc0d3s <brc0d3s@gmail.com>
2024-03-13 18:30:14 +0300

    User Receipt Printing Implemented

--- E-MEDICAL MANAGEMENT/Dispaly.vb
+++ E-MEDICAL MANAGEMENT/Dispaly.vb
@@ -1,10 +1,16 @@
-﻿Imports Npgsql
+﻿Imports System.Drawing.Printing
+Imports Npgsql
 
 Public Class Dispaly
     Public Property i As Integer = 0
     Dim test As Integer = 0
     Dim general As New DataTable
     Private connection As New NpgsqlConnection(GetConnectionString())
+
+    'Printing variables
+    Dim WithEvents PD As New PrintDocument
+    Dim PPD As New PrintPreviewDialog
+    Dim Longpaper As Integer
 
     Private Sub Dispaly_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         lblName1.Text = StartPage.userName.ToString()
@@ -46,9 +52,72 @@ Public Class Dispaly
         Me.Close()
     End Sub
 
+
+    ' Print functionalities
+    Private Sub PD_BeginPrint(sender As Object, e As Printing.PrintEventArgs) Handles PD.BeginPrint
+        Dim pagesetup As New PageSettings
+        pagesetup.PaperSize = New PaperSize("Custom", 500, 700) ' Adjusted paper size
+        PD.DefaultPageSettings = pagesetup
+    End Sub
+
+    Private Sub PD_PrintPage(sender As Object, e As PrintPageEventArgs) Handles PD.PrintPage
+        Dim f8 As New Font("Calibri", 8, FontStyle.Regular)
+        Dim f10 As New Font("Calibri", 10, FontStyle.Regular)
+        Dim f10b As New Font("Calibri", 10, FontStyle.Bold)
+        Dim f14 As New Font("Calibri", 14, FontStyle.Bold)
+
+        Dim leftmargin As Integer = 50 ' Adjusted left margin
+
+        ' Booking data
+        Dim bookingDate As DateTime = DateTime.Now
+        Dim service As String = grpDetails.Text
+        Dim DocCenterName As String = txtName.Text
+        Dim consultDate As Date = dtpkConsult.Value
+        Dim Address As String = txtAddress.Text
+        Dim Phone As String = txtPhone.Text
+        Dim Ohours As String = txtTime.Text
+
+        ' Define string formats for alignment
+        Dim leftFormat As New StringFormat()
+        leftFormat.Alignment = StringAlignment.Near
+
+        ' Define spacing between lines
+        Dim lineHeight As Integer = 20
+        Dim startY As Integer = 50 ' Starting position for drawing
+
+        ' Draw Header
+        e.Graphics.DrawString("VertexHub E-Medical", f14, Brushes.RoyalBlue, leftmargin, startY)
+        startY += lineHeight
+        e.Graphics.DrawString("Medical Appointment Receipt", f14, Brushes.Black, leftmargin, startY)
+        startY += lineHeight * 2 ' Add extra space after header
+
+        ' Draw User Information
+        e.Graphics.DrawString("User ID: " & StartPage.userID, f10b, Brushes.Black, leftmargin, startY)
+        startY += lineHeight
+        e.Graphics.DrawString("User Name: " & StartPage.userName, f10b, Brushes.Black, leftmargin, startY)
+        startY += lineHeight * 2 ' Add extra space after user info
+
+        ' Draw Booking Information
+        e.Graphics.DrawString("Booking Date: " & bookingDate.ToString("dd/MM/yyyy"), f10b, Brushes.Black, leftmargin, startY)
+        startY += lineHeight
+        e.Graphics.DrawString("Consultation Date: " & consultDate.ToString("dd/MM/yyyy"), f10b, Brushes.Black, leftmargin, startY)
+        startY += lineHeight
+        e.Graphics.DrawString("Service: " & service, f10b, Brushes.Black, leftmargin, startY)
+        startY += lineHeight
+        e.Graphics.DrawString("Doctor/Facility Name: " & DocCenterName, f10b, Brushes.Black, leftmargin, startY)
+        startY += lineHeight
+        e.Graphics.DrawString("Doctor/Facility Address: " & Address, f10b, Brushes.Black, leftmargin, startY)
+        startY += lineHeight
+        e.Graphics.DrawString("Phone: " & Phone, f10b, Brushes.Black, leftmargin, startY)
+        startY += lineHeight
+        e.Graphics.DrawString("Operation Hours: " & Ohours, f10b, Brushes.Black, leftmargin, startY)
+    End Sub
+
+
     Private Sub btnBook_Click(sender As Object, e As EventArgs) Handles btnBook.Click
         'Printing to be implemented after booking
         Try
+            'Booking data
             Dim bookingDate As DateTime = DateTime.Now
             Dim service As String = grpDetails.Text
             Dim DocCenterName As String = txtName.Text
@@ -56,8 +125,6 @@ Public Class Dispaly
             Dim Address As String = txtAddress.Text
             Dim Phone As String = txtPhone.Text
             Dim Ohours As String = txtTime.Text
-
-
 
             connection.Open()
             Using cmd As New NpgsqlCommand("INSERT INTO bookings(booking_date, user_id, user_name, service, doctor_or_center, consultation_date,  address, operation_hours, phone) VALUES(@bookingDate, @UserId, @Name, @service, @DocCenterName, @consultDate, @Address, @OperationHours, @Phone)", connection)
@@ -76,6 +143,8 @@ Public Class Dispaly
 
             If (i > 0) Then
                 MsgBox("Service Successfully Booked! Please Wait Printing your Receipt")
+                PPD.Document = PD
+                PPD.ShowDialog()
             End If
         Catch ex As Exception
             MsgBox("Error: " & ex.Message)
@@ -83,5 +152,4 @@ Public Class Dispaly
             connection.Close()
         End Try
     End Sub
-
 End Class
